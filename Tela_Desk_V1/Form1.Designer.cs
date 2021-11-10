d4b4eb4eef5356d1f24b26c62b7cb98da869a482
Zeusc <Zeusc@DESKTOP-0P02U63>
2021-11-10 09:43:33 -0300

    Adicionar arquivos de projeto.

---
+++ Tela_Desk_V1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Tela_Desk_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.TxtTelefone = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.IbAgenda = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefone: ";
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(79, 12);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(400, 22);
+            this.TxtNome.TabIndex = 2;
+            // 
+            // TxtTelefone
+            // 
+            this.TxtTelefone.Location = new System.Drawing.Point(93, 43);
+            this.TxtTelefone.Name = "TxtTelefone";
+            this.TxtTelefone.Size = new System.Drawing.Size(218, 22);
+            this.TxtTelefone.TabIndex = 3;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(27, 82);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(108, 82);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 5;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click_1);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(189, 82);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click_1);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(270, 82);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 7;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click_1);
+            // 
+            // IbAgenda
+            // 
+            this.IbAgenda.FormattingEnabled = true;
+            this.IbAgenda.ItemHeight = 16;
+            this.IbAgenda.Location = new System.Drawing.Point(27, 112);
+            this.IbAgenda.Name = "IbAgenda";
+            this.IbAgenda.Size = new System.Drawing.Size(318, 260);
+            this.IbAgenda.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 555);
+            this.Controls.Add(this.IbAgenda);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.TxtTelefone);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Agenda de Telefones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.TextBox TxtTelefone;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.ListBox IbAgenda;
+    }
+}
+
---
+++ Tela_Desk_V1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using aplicativoMVC_V1.controle;
+using aplicativoMVC_V1.modelo;
+
+namespace Tela_Desk_V1
+{
+    public partial class Form1 : Form
+    {
+        private Gerenciador gerenciador;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gerenciador = new Gerenciador(BancoDeDados.SqlServer);
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            List<Pessoa> pessoas;
+            if (TxtNome.Text.Equals(""))
+            {
+                pessoas = gerenciador.dao.Consulte();
+            }
+            else
+            {
+                pessoas = gerenciador.dao.Consulte(TxtNome.Text);
+            }
+            IbAgenda.Items.Clear();
+            foreach (Pessoa p in pessoas)
+            {
+                IbAgenda.Items.Add(p.ToString());
+            }
+        }
+
+        private void btnInserir_Click_1(object sender, EventArgs e)
+        {
+            Pessoa p = new Pessoa();
+            p.nome = TxtNome.Text;
+            p.telefone = Convert.ToInt32(TxtTelefone.Text);
+            gerenciador.dao.Insira(p);
+            TxtNome.Text = "";
+            TxtTelefone.Text = "";
+        }
+
+        private void btnAlterar_Click_1(object sender, EventArgs e)
+        {
+            Pessoa p = new Pessoa();
+            p.nome = TxtNome.Text;
+            p.telefone = Convert.ToInt32(TxtTelefone.Text);
+            gerenciador.dao.Altere(p);
+        }
+
+        private void btnExcluir_Click_1(object sender, EventArgs e)
+        {
+            Pessoa p = new Pessoa();
+            p.nome = TxtNome.Text;
+            p.telefone = Convert.ToInt32(TxtTelefone.Text);
+            gerenciador.dao.Exclua(p);
+        }
+    }
+}
